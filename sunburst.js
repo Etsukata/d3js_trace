dd6f6e70620b1b123d0bb2fc04198ab91f04ddce
Eiichi Tsukata <devel@etsukata.com>
2013-10-26 17:01:15 +0900

    split trace json file

--- sunburst.js
+++ sunburst.js
@@ -5,7 +5,7 @@ var width = 960,
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
 
-var y = d3.scale.sqrt()
+var y = d3.scale.linear()
     .range([0, radius]);
 
 var color = d3.scale.category20c();
@@ -25,18 +25,44 @@ var arc = d3.svg.arc()
     .innerRadius(function(d) { return Math.max(0, y(d.y)); })
     .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
-root = flare_json;
-var path = svg.selectAll("path")
+//root = flare_json;
+root = test_json;
+console.log(root);
+var g = svg.selectAll("g")
     .data(partition.nodes(root))
-  .enter().append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-    .on("click", click);
+  .enter().append("g");
+
+var path = g.append("path")
+  .attr("d", arc)
+  .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+  .on("click", click);
+
+var text = g.append("text")
+  .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+  .attr("x", function(d) { return y(d.y); })
+  .attr("dx", "6") // margin
+  .attr("dy", ".35em") // vertical-align
+  .text(function(d) { return d.name; });
 
 function click(d) {
+  // fade out all text elements
+  text.transition().attr("opacity", 0);
+
   path.transition()
     .duration(750)
-    .attrTween("d", arcTween(d));
+    .attrTween("d", arcTween(d))
+    .each("end", function(e, i) {
+        // check if the animated element's data e lies within the visible angle span given in d
+        if (e.x >= d.x && e.x < (d.x + d.dx)) {
+          // get a selection of the associated text element
+          var arcText = d3.select(this.parentNode).select("text");
+          // fade in the text element and recalculate positions
+          arcText.transition().duration(750)
+            .attr("opacity", 1)
+            .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+            .attr("x", function(d) { return y(d.y); });
+        }
+    });
 }
 
 d3.select(self.frameElement).style("height", height + "px");
@@ -53,3 +79,6 @@ function arcTween(d) {
   };
 }
 
+function computeTextRotation(d) {
+  return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+}
